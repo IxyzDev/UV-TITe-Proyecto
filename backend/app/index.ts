7ab3eea471542ac7901422d06b3da8bb67df6bbd
Ixyz.dev <kevin.diaz@alumnos.uv.cl>
2023-11-13 17:28:36 -0300

    variables de entorno configuradas

--- backend/app/index.ts
+++ backend/app/index.ts
@@ -1,34 +1,37 @@
-import express from 'express';
-import cors from 'cors';
-import db from './models';
+import express from "express";
+import cors from "cors";
+import db from "./models";
 
 // Importación de las rutas
-import funcionarioRouter from './routes/funcionario.routes';
-import movilRouter from './routes/movil.routes';
-import comunicacionRouter from './routes/comunicacion.routes';
-import ubicacionRouter from './routes/ubicacion.routes';
-import patrullero from './routes/patrullero.routes';
-import operador from './routes/operador.routes';
-import asignacionPM from './routes/asig.pat.mov.routes';
-import reporteRouter from './routes/reporte.routes';
-import asignacionPR from './routes/asig.pat.rep.routes';
+import funcionarioRouter from "./routes/funcionario.routes";
+import movilRouter from "./routes/movil.routes";
+import comunicacionRouter from "./routes/comunicacion.routes";
+import ubicacionRouter from "./routes/ubicacion.routes";
+import patrullero from "./routes/patrullero.routes";
+import operador from "./routes/operador.routes";
+import asignacionPM from "./routes/asig.pat.mov.routes";
+import reporteRouter from "./routes/reporte.routes";
+import asignacionPR from "./routes/asig.pat.rep.routes";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json()); // middleware que transforma el req.body a JSON
 
-db.sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Se escucha en el puerto ${PORT}`);
-  });
-}).catch((err: Error) => console.error('Error al sincronizar la base de datos:', err));
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Se escucha en el puerto ${PORT}`);
+    });
+  })
+  .catch((err: Error) =>
+    console.error("Error al sincronizar la base de datos:", err)
+  );
 
-
-
-app.get('/', (_req, res) => {
-  res.json({ message: 'FELICIDADES LOGRASTE SER FELIZ' });
+app.get("/", (_req, res) => {
+  res.json({ message: "FELICIDADES LOGRASTE SER FELIZ" });
 });
 
 app.use("/funcionario", funcionarioRouter);
--- backend/app/models/index.ts
+++ backend/app/models/index.ts
@@ -1,38 +1,49 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { Sequelize, DataTypes } from 'sequelize';
+import * as fs from "fs";
+import * as path from "path";
+import { Sequelize, DataTypes } from "sequelize";
+
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'home';
-const config = require(__dirname + '/../config/db.config')[env];
 const db: any = {};
 
-let sequelize: Sequelize;
+const username = process.env.PRODUCTION_DB_USERNAME || "defaultUsername";
+const password = process.env.PRODUCTION_DB_PASSWORD || "defaultPassword";
+const host = process.env.PRODUCTION_DB_HOST || "localhost";
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]!, config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize({
+  username,
+  password,
+  host,
+  database: "test",
+  dialect: "mysql",
+});
 
-console.log('Todos los archivos en el directorio:', fs.readdirSync(__dirname));
+console.log(sequelize);
+
+//console.log("Todos los archivos en el directorio:", fs.readdirSync(__dirname));
 
 const modelFiles = fs.readdirSync(__dirname).filter((file: string) => {
-  return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.ts');
+  return (
+    file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".ts"
+  );
 });
-console.log('Archivos de modelo filtrados:', modelFiles);
+//console.log("Archivos de modelo filtrados:", modelFiles);
 
 modelFiles.forEach((file: any) => {
-  console.log(`Importando modelo desde el archivo: ${file}`);
+  //console.log(`Importando modelo desde el archivo: ${file}`);
   const modelModule = require(path.join(__dirname, file));
-  const model = modelModule.default ? modelModule.default(sequelize, DataTypes) : modelModule(sequelize, DataTypes);
-  console.log(`Modelo importado: ${model.name}`);
+  const model = modelModule.default
+    ? modelModule.default(sequelize, DataTypes)
+    : modelModule(sequelize, DataTypes);
+  //console.log(`Modelo importado: ${model.name}`);
   db[model.name] = model;
 });
 
-console.log('Modelos importados:', Object.keys(db));
+//console.log("Modelos importados:", Object.keys(db));
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
