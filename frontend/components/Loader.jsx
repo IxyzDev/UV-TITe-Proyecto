ad060c6b64e661569356472ecc67fffe7b15e8b0
Ixyz.dev <kevin.diaz@alumnos.uv.cl>
2023-12-04 08:38:54 -0300

    Error en places use client es requerido

--- frontend/components/Loader.jsx
+++ frontend/components/Loader.jsx
@@ -1,17 +1,16 @@
-"use client";
-import "../styles/Loader.css"
+import "../styles/Loader.css";
 
 const Loader = () => {
-    return (
-        <div className="flex justify-center m-20">
-            <div className="lds-ring">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center m-20">
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
--- frontend/components/Places.jsx
+++ frontend/components/Places.jsx
@@ -1,68 +1,100 @@
-import React from 'react';
+"use client";
 import { useLoadScript } from "@react-google-maps/api";
-import usePlacesAutocomplete, { getGeocode, getLatLng, } from "use-places-autocomplete";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 import { TextField } from "@mui/material";
 import AutocompleteMUI from "@mui/material/Autocomplete";
 
 const apiKey = "AIzaSyBiEhWbQVjxhbz-zS2PqssIpf_rqIpcmBw"; // Reemplazar API KEY
 
-export default function Places({ formulario, setFormulario,error, helperText }) {
-	const { isLoaded } = useLoadScript({
-		googleMapsApiKey: apiKey,
-		libraries: ["places"],
-	});
+export default function Places({
+  formulario,
+  setFormulario,
+  error,
+  helperText,
+}) {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: apiKey,
+    libraries: ["places"],
+  });
 
-	if (!isLoaded)
-		return <div>Cargando...</div>;
-	return <PlacesAutocomplete formulario={formulario} setFormulario={setFormulario} error={error} helperText={helperText}  />
+  if (!isLoaded) return <div>Cargando...</div>;
+  return (
+    <PlacesAutocomplete
+      formulario={formulario}
+      setFormulario={setFormulario}
+      error={error}
+      helperText={helperText}
+    />
+  );
 }
 
 function PlacesAutocomplete({ formulario, setFormulario, error, helperText }) {
-	const { labelAddress, setValue, suggestions: { data }, clearSuggestions, } = usePlacesAutocomplete();
+  const {
+    labelAddress,
+    setValue,
+    suggestions: { data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
 
-	const handleSelect = async (address) => {
-		if (address && address.label) {
-			address = address.label;
-			setValue(address, false);
-			clearSuggestions();
-			const results = await getGeocode({ address });
-			const { lat, lng } = await getLatLng(results[0]);
-			const coord = lat + "," + lng;
-			setFormulario(formulario => ({ ...formulario, "direccion": direccion.value }));
-			setFormulario(formulario => ({ ...formulario, "coordenadas": coord }));
-		}
-	};
-	
+  const handleSelect = async (address) => {
+    if (address && address.label) {
+      address = address.label;
+      setValue(address, false);
+      clearSuggestions();
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      const coord = lat + "," + lng;
+      setFormulario((formulario) => ({
+        ...formulario,
+        direccion: direccion.value,
+      }));
+      setFormulario((formulario) => ({ ...formulario, coordenadas: coord }));
+    }
+  };
 
-	const addressOptions = data.map(({ place_id, description }) => ({
-		label: description,
-		id: place_id,
-	}));
+  const addressOptions = data.map(({ place_id, description }) => ({
+    label: description,
+    id: place_id,
+  }));
 
-	const handleChange = (event, selected) => {
-		handleSelect(selected); // Llama a handleSelect con el nuevo valor
-	};
+  const handleChange = (event, selected) => {
+    handleSelect(selected); // Llama a handleSelect con el nuevo valor
+  };
 
-	return (
-		<>
-			<div className="col-span-3">
-				<label htmlFor="direccion" className="block text-sm pl-1 pb-3 font-medium text-gray-700" >
-					{" "} Dirección:{" "}
-				</label>
-				<AutocompleteMUI disablePortal fullWidth id="direccion"
-					value={labelAddress}
-					onChange={handleChange}
-					options={addressOptions}
-					renderInput={(params) =>
-						<TextField required {...params}
-							error={error} // Utiliza el prop error para mostrar un estado de error
-							helperText={helperText} // Utiliza el prop helperText para mostrar el mensaje de error
-							label="Dirección de eventualidad"
-							onChange={(e) => { setValue(e.target.value); }}
-						/>
-					}
-				/>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="col-span-3">
+        <label
+          htmlFor="direccion"
+          className="block text-sm pl-1 pb-3 font-medium text-gray-700"
+        >
+          {" "}
+          Dirección:{" "}
+        </label>
+        <AutocompleteMUI
+          disablePortal
+          fullWidth
+          id="direccion"
+          value={labelAddress}
+          onChange={handleChange}
+          options={addressOptions}
+          renderInput={(params) => (
+            <TextField
+              required
+              {...params}
+              error={error} // Utiliza el prop error para mostrar un estado de error
+              helperText={helperText} // Utiliza el prop helperText para mostrar el mensaje de error
+              label="Dirección de eventualidad"
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+            />
+          )}
+        />
+      </div>
+    </>
+  );
 }
